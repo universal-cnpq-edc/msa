16f1a4678468f1f23b6463d2e0b141e809c3f9c0
hrazente <humberto.razente@gmail.com>
2024-06-20 17:42:12 -0300

    Adicionado Projeto e Makefile Netbeans msa-nb

---
+++ msa-nb/nbproject/Makefile-Debug.mk
@@ -0,0 +1,89 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/75fcdad/malloc_count.o \
+	${OBJECTDIR}/_ext/5c0/main.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ../main
+
+../main: ${OBJECTFILES}
+	${MKDIR} -p ..
+	${LINK.cc} -o ../main ${OBJECTFILES} ${LDLIBSOPTIONS} -I/home/vb/include -L/home/vb/lib -lsdsl -ldivsufsort -ldivsufsort64
+
+${OBJECTDIR}/_ext/75fcdad/malloc_count.o: ../external/malloc_count/malloc_count.c
+	${MKDIR} -p ${OBJECTDIR}/_ext/75fcdad
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/75fcdad/malloc_count.o ../external/malloc_count/malloc_count.c
+
+${OBJECTDIR}/_ext/5c0/main.o: ../main.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/5c0
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../../../include -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/5c0/main.o ../main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
