5cfeb89f79380661e38dcf847fd95b207de24fe5
hrazente <humberto.razente@gmail.com>
2024-06-21 23:18:37 -0300

    compare script

--- util/compare/run.sh
+++ util/compare/run.sh
@@ -27,6 +27,6 @@ echo "=========================================="
 echo "head $1.msa.out"
 head $1.msa.out
 echo "=========================================="
-echo "./compare $1.txt $1.exact.out $1.msa.out $4" > $1.compare.txt
-./compare $1.txt $1.exact.out $1.msa.out $4 >> $1.compare.txt
-cat $1.compare.txt
+echo "./compare $1.txt $1.exact.out $1.msa.out $4" > $1-$2-$3-$4.compare.txt
+./compare $1.txt $1.exact.out $1.msa.out $4 >> $1-$2-$3-$4.compare.txt
+cat $1-$2-$3-$4.compare.txt
